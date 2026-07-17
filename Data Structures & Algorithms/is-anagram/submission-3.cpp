bfcb44c9cf2d01d656659bf2ebd7a6aaa6aef2b4
Mayank Sarode <hacksm057@gmail.com>
2026-07-17 11:02:37 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        vector<int> c(26, 0);
+        
+        for(int i = 0; i < s.size(); i++){
+            c[s[i]-'a']++;
+            c[t[i]-'a']--;
+        }
+        for(int oi: c){
+            if(oi!=0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
